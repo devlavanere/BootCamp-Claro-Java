5c6de3e3bf3d09e11af940a42da5bd3da5e2681c
Michel Lavanere <michel.lavanere@academico.ifpb.edu.br>
2024-08-25 04:57:52 -0300

    Concluindo desafio ContaTerminal .

--- src/sintaxe_basica_java/desafio/ContaTerminal.java
+++ src/sintaxe_basica_java/desafio/ContaTerminal.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TODO: Conhecer e importar a classe Scanner
-        //TODO: Exibir as mensagens para o nosso usuário
-        //TODO: Obter pela Scanner os valores digitados no terminal
-        //TODO: Exibir mensagem da conta criada
 
         String nome;
         String agencia;
